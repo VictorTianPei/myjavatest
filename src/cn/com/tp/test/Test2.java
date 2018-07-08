c8ec2e0681e254aeff8b961b06224c8169cada93
tianpei <478090958@qq.com>
2018-07-08 10:07:46 +0800

    test2

---
+++ src/cn/com/tp/test/Test2.java
@@ -0,0 +1,35 @@
+/**   
+* @Title: Test2.java 
+* @Package cn.com.tp.test 
+* @Description: TODO(用一句话描述该文件做什么) 
+* @author A18ccms A18ccms_gmail_com   
+* @date 2018年7月8日 上午10:02:06 
+* @version V1.0   
+*/
+package cn.com.tp.test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * ## 类说明：
+ * 	描述
+ * @author    [tianpei]   
+ * @version   [V1.0, 2018年7月8日]
+ * @package cn.com.tp.test
+ * @since
+日期|作者|类型|内容
+:----|:-----|:-----|:-----
+2018年7月8日|tianpei|新增|Test2.java新增
+*/
+public class Test2 {
+    public static void main(String[] args) {
+        List<String> lista = new ArrayList<String>();
+        List<Integer> listb = new ArrayList<Integer>();
+        lista.add("1");
+        listb.add(12);
+        System.out.println(lista);
+        System.out.println(listb);
+        System.out.println(lista.getClass() ==  listb.getClass());
+    }
+}
